22ab0cc87248c4dd74322faee3622264d9ef6068
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-03 21:17:12 -0500

    Updated Notebooks

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,9 +5,11 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* shapely descartes
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* 
+conda install -y shapely=1.6* descartes=1.1*
+conda install -y -c conda-forge scipy=1.2*
 
 pip install --upgrade pip
-pip install pandas==0.24.2
+pip install pandas==0.24.2 alphashape
 
 source /home/nbuser/anaconda3_420/bin/deactivate
