3be13e0b4daab69f1ece74fe046ae0789cc52094
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-03 21:34:33 -0500

    Updated Binder and Azure scripts

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -6,7 +6,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 # Install packages
 conda update -c conda-forge conda conda-build
 conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* pandas=0.25*
-conda install -y shapely=1.6* descartes=1.1*
+conda install -y shapely=1.6* descartes=1.1* geopandas=0.5*
 conda install -y -c conda-forge scipy=1.2*
 
 pip install --upgrade pip
