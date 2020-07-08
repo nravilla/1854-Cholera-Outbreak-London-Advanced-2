f82fb858581d5f7ee9c7506905b66464db569c3e
Herman Tolentino <herman.tolentino@gmail.com>
2020-07-08 06:51:53 -0500

    updated setup binder and azure scripts

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,9 +5,9 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* pandas=0.25*
-conda install -y shapely=1.6* descartes=1.1* geopandas=0.5*
-conda install -y -c conda-forge scipy=1.2*
+conda install -y -c conda-forge folium=0.11* jinja2=2.10* osmnx=0.15* networkx pandas=1.0*
+conda install -y shapely descartes geopandas=0.80*
+conda install -y -c conda-forge scipy
 
 pip install --upgrade pip
 pip install alphashape
